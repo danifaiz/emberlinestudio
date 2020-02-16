b5ad2b841d3b02fe29fb9ef355f921b35fd67f0f
AbrarAhmedFaiz <60220117+AbrarAhmedFaiz@users.noreply.github.com>
2020-02-16 22:18:07 +0500

    IsSafari userAgent

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -34,8 +34,9 @@ export default class Projects extends Component {
           });
     }
     imageBrowserCompatible(url) {
+        var userAgent = window.navigator.userAgent;
         var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof window['safari'] !== 'undefined' && window['safari'].pushNotification));
-        if(isSafari) {
+        if(isSafari || userAgent.match(/iPad/i) || userAgent.match(/iPhone/i)) {
             return url.replace(".webp",".png")
         } else {
             return url;
